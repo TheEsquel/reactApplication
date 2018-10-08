f79f16edec99ce3972a9e675deb41918840129db
TheEsquel <rem.asadullin@gmail.com>
2018-10-08 19:59:45 +0500

    setting up webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let myUser = require('./user');
+
+myUser('Anna' , 'anna@gmail.com' ,  '8 800 123');
+
+//exports.myUser = myUser; 
---
+++ src/user.js
@@ -0,0 +1,6 @@
+'use strict' ;
+
+module.exports = function(name, email,  phone) {
+    console.log(`Имя  ${name},  email  ${email},  телефон  ${phone}`);
+
+}
