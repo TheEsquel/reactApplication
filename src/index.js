321edd6ef8beedb483763b72369ab424bb1b071c
TheEsquel <rem.asadullin@gmail.com>
2018-10-10 03:07:03 +0500

    setting up webpack.config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 'use strict';
 
-let myUser = require('./user');
+// let myUser = require('./user');
 
-myUser('Anna' , 'anna@gmail.com' ,  '8 800 123');
+// myUser('Anna' , 'anna@gmail.com' ,  '8 800 123');
 
-//exports.myUser = myUser; 
+// exports.myUser = myUser; 
+
+import average from './user'
+import $ from 'jquery'
+
+let getRandomNumbers = (length, max) => 
+    {
+        let randomNumbers = []
+        for (let i = 0; i < length ; i++)
+            {
+                randomNumbers.push(Math.floor(Math.random() * max))
+            }
+        return randomNumbers    
+    }
+let randomNumbers = getRandomNumbers(10000, 100)
+
+console.log('These are some random numbers: ' + randomNumbers);
+// console.log(`randomNumbers is a ${typeof randomNumbers}`)
+// console.log(`average is a ${typeof average}`)
+console.log('and  this is the average for them is ' + average(randomNumbers))
+
+$('.sum').text('These are some random numbers: ' + randomNumbers)
+$('.average').text('and  this is the average for them is ' + average(randomNumbers))
--- src/user.js
+++ src/user.js
@@ -1,6 +1,21 @@
-'use strict' ;
+// 'use strict' 
 
-module.exports = function(name, email,  phone) {
-    console.log(`Имя  ${name},  email  ${email},  телефон  ${phone}`);
+// module.exports = function(name, email,  phone) {
+//     console.log(`Имя  ${name},  email  ${email},  телефон  ${phone}`);
 
-}
+// };
+
+function sum (numbers)
+    {
+        let sum = 0
+        numbers.forEach(element => {
+            sum+= element
+        });
+        return sum;
+    }
+function average(numbers)
+    {
+        return sum(numbers) / numbers.length;
+    }   
+     
+export default average;    
